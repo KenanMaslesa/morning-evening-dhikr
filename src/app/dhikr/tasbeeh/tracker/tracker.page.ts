fc899dc6d1d93c049c8bdd2c1ea73366fe7de8a4
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-03-05 22:11:19 +0100

    chart

--- src/app/dhikr/tasbeeh/tracker/tracker.page.ts
+++ src/app/dhikr/tasbeeh/tracker/tracker.page.ts
@@ -1,17 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TasbeehService } from '../tasbeeh.service';
-
+import { Chart, registerables } from 'chart.js'; Chart.register(...registerables);
 @Component({
   selector: 'app-tracker',
   templateUrl: './tracker.page.html',
   styleUrls: ['./tracker.page.scss'],
 })
-export class TrackerPage implements OnInit {
+export class TrackerPage implements OnInit, AfterViewInit {
+  @ViewChild('barChart') barChart;
   dhikrs: any;
+  bars: any;
+  themeColor: any;
   constructor(public tasbeehService: TasbeehService) { }
 
   ngOnInit() {
     this.dhikrs = this.tasbeehService.dhikrsFromStorage;
+
+    const themeColor = localStorage.getItem('theme');
+    if (themeColor) {
+        this.themeColor = JSON.parse(themeColor);
+    }
   }
 
+  ngAfterViewInit() {
+    this.daily();
+  }
+
+  createBarChart(labels: any[], data: any[]) {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'line',//bar
+      data: {
+        labels,
+        datasets: [{
+          label: 'Zikr',
+          data,
+          backgroundColor: this.themeColor,
+          borderColor: 'gray',
+          borderWidth: 1,
+          fill: true
+        }]
+      },
+      // options: {
+      //   indexAxis: 'y',
+      // }
+      options: {
+        scales: {
+          y: {
+              beginAtZero: true
+          }
+      }
+      },
+    });
+  }
+
+  daily(){
+    const dates = this.dhikrs.map(item => item.date);
+    const counters = this.dhikrs.map(item => {
+      let total = 0;
+      item.dhikrs.forEach(element => {
+        total += element.counter;
+      });
+      return total;
+    });
+    this.createBarChart(dates, counters);
+  }
 }
---
+++ src/app/shared/chart.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Injectable({providedIn: 'root'})
+export class ChartService {
+  constructor() { }
+
+}
