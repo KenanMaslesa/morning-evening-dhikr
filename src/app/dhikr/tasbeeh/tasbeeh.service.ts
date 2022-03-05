11bec871f9ac4573eae46b9c297579b06f238cb9
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-03-05 23:07:22 +0100

    .

--- src/app/dhikr/tasbeeh/tasbeeh.service.ts
+++ src/app/dhikr/tasbeeh/tasbeeh.service.ts
@@ -5,29 +5,46 @@ export class TasbeehService {
   selectedDhikr: any;
   dhikrs = [
     {
+      arabic: 'لَٓا إِلٰهَ إِلَّا اللّٰهُ',
+      bosnian: 'Nema boga osim Allaha',
+      counter: 0,
+      favorite: false
+    },
+    {
       arabic: 'سُبْحَانَ اللَّهِ',
       bosnian: 'Slavljen neka je Allah',
       counter: 0,
+      favorite: false
     },
     {
       arabic: 'الْحَمْدُ لِلَّهِ',
       bosnian: 'Hvala Allahu',
       counter: 0,
+      favorite: false
     },
     {
       arabic: 'اللَّهُ أَكْبَرُ',
       bosnian: 'Allah je najveci',
       counter: 0,
+      favorite: false
     },
     {
-      arabic: 'اللَّهُــمَّ صَلِّ وَسَـــلِّمْ على نَبِيِّنَـــا مُحمَّد ﷺ ',
+      arabic: 'أَسْتَغْفِرُ ٱللَّٰهَ',
+      bosnian: 'Allahu moj oprosti mi',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic: 'اللَّهُــمَّ صَلِّ وَسَـــلِّمْ على نَبِيِّنَـــا مُحمَّد',
       bosnian: 'O Allahu, blagoslovi našeg poslanika Muhammeda ﷺ.',
       counter: 0,
+      favorite: false
     },
     {
       arabic: 'سُبْحَانَ اللَّهِ وَبِحَمْدِه',
       bosnian: 'Slavljen je Allah i Njemu pripada sva hvala',
       counter: 0,
+      favorite: false
     },
     {
       arabic:
@@ -35,11 +52,77 @@ export class TasbeehService {
       bosnian:
         'Nema boga osim Allaha, Jedinoga, Koji druga nema. Njemu pripada vlast nad svim i sva pohvala. On sve može.',
       counter: 0,
+      favorite: false
+    },
+    {
+      arabic: 'أَسْتَغْفِرُ اللّٰهَ الْعَظِیْمَ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic: 'لَٓا حَوْلَ وَلَا قُوَّةَ إِلَّا بِاللّٰهِ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic: 'لَٓا إِلَهَ إِلّاَ اللّٰهُ مُحَمَّدٌ رَسُوْلُ اللّٰهِ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic: 'اَللّٰهُمَّ صَلِّ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic: 'سُبْحَانَ اللّٰهِ وَ بِحَمْدِهِ ، سُبْحَانَ اللّٰهِ الْعَظِيْمِ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic:
+        'سُبْحَانَ اللّٰهِ ، وَالْحَمْدُلِلّٰهِ ، وَ لَٓا إِلَهَ إِلّاَ اللّٰهُ ، وَ اللّٰهُ أَكْبَرُ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic:
+        'اَللّٰهُمَّ إِنَّكَ عَفُوٌّ كَرِيْمٌ تُحِبُّ الْعَفْوَ فَاعْفُ عَنِّي',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic:
+        'لَٓا إِلَهَ إِلّاَ أَنْتَ سُبْحَانَكَ إِنِّي كُنْتُ مِنَ الظَّالِمِيْنَ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic:
+        'أَسْتَغْفِرُ اللّٰهَ الْعَظِيْمَ الَّذِي لَٓا إِلَهَ إِلاَّ هُوَ الْحَيُّ الْقَيُّوْمُ وَ أَتُوْبُ إِلَيْهِ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
+    },
+    {
+      arabic:
+        'اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ، كَمَا صَلَّيْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ، إِنَّكَ حَمِيدٌ مَجِيدٌ  اللَّهُمَّ بَارِكْ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ، كَمَا بَارَكْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ، إِنَّكَ حَمِيدٌ مَجِيدٌ',
+      bosnian: '',
+      counter: 0,
+      favorite: false
     },
   ];
   dhikrsFromStorage = [];
   constructor() {
-    this.selectedDhikr = {counter: 0};
+    this.selectedDhikr = { counter: 0 };
 
     const dhikrsFromStorage = localStorage.getItem('dhikrs');
     if (dhikrsFromStorage) {
@@ -49,11 +132,10 @@ export class TasbeehService {
       this.dhikrsFromStorage.filter((item) => {
         const currentDate = this.getCurrentDateAsString(new Date());
         if (item.date === currentDate) {
-          this.dhikrs = item.dhikrs;
+          this.dhikrs = item.dhikrs.sort((a, b) => b.favorite - a.favorite);;
         }
       });
-    }
-    else {
+    } else {
       const obj = {
         date: this.getCurrentDateAsString(new Date()),
         dhikrs: this.dhikrs,
@@ -102,6 +184,16 @@ export class TasbeehService {
 
   resetCounter() {
     this.selectedDhikr.counter = -1;
+    localStorage.setItem('dhikrs', JSON.stringify(this.dhikrsFromStorage));
+    localStorage.setItem('selectedDhikr', JSON.stringify(this.selectedDhikr));
+  }
+
+  toggleFavorite(dhikr, value: boolean) {
+    this.dhikrs.forEach(item => {
+      if(item.arabic === dhikr.arabic){
+        item.favorite = value? true: false;
+      }
+    });
     localStorage.setItem('dhikrs', JSON.stringify(this.dhikrsFromStorage));
     localStorage.setItem('selectedDhikr', JSON.stringify(this.selectedDhikr));
   }
