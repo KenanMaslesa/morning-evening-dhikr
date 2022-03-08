b05c9fe45f87fbb4df2f31646eef18a67021c991
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-03-08 10:52:37 +0100

    .

--- src/app/dhikr/tasbeeh/tasbeeh.service.ts
+++ src/app/dhikr/tasbeeh/tasbeeh.service.ts
@@ -8,43 +8,43 @@ export class TasbeehService {
       arabic: 'لَٓا إِلٰهَ إِلَّا اللّٰهُ',
       bosnian: 'Nema boga osim Allaha',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'سُبْحَانَ اللَّهِ',
       bosnian: 'Slavljen neka je Allah',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'الْحَمْدُ لِلَّهِ',
       bosnian: 'Hvala Allahu',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'اللَّهُ أَكْبَرُ',
       bosnian: 'Allah je najveci',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'أَسْتَغْفِرُ ٱللَّٰهَ',
       bosnian: 'Allahu moj oprosti mi',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'اللَّهُــمَّ صَلِّ وَسَـــلِّمْ على نَبِيِّنَـــا مُحمَّد',
       bosnian: 'O Allahu, blagoslovi našeg poslanika Muhammeda ﷺ.',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'سُبْحَانَ اللَّهِ وَبِحَمْدِه',
       bosnian: 'Slavljen je Allah i Njemu pripada sva hvala',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic:
@@ -52,72 +52,72 @@ export class TasbeehService {
       bosnian:
         'Nema boga osim Allaha, Jedinoga, Koji druga nema. Njemu pripada vlast nad svim i sva pohvala. On sve može.',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'أَسْتَغْفِرُ اللّٰهَ الْعَظِیْمَ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'لَٓا حَوْلَ وَلَا قُوَّةَ إِلَّا بِاللّٰهِ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'لَٓا إِلَهَ إِلّاَ اللّٰهُ مُحَمَّدٌ رَسُوْلُ اللّٰهِ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'اَللّٰهُمَّ صَلِّ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic: 'سُبْحَانَ اللّٰهِ وَ بِحَمْدِهِ ، سُبْحَانَ اللّٰهِ الْعَظِيْمِ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic:
         'سُبْحَانَ اللّٰهِ ، وَالْحَمْدُلِلّٰهِ ، وَ لَٓا إِلَهَ إِلّاَ اللّٰهُ ، وَ اللّٰهُ أَكْبَرُ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic:
         'اَللّٰهُمَّ إِنَّكَ عَفُوٌّ كَرِيْمٌ تُحِبُّ الْعَفْوَ فَاعْفُ عَنِّي',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic:
         'لَٓا إِلَهَ إِلّاَ أَنْتَ سُبْحَانَكَ إِنِّي كُنْتُ مِنَ الظَّالِمِيْنَ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic:
         'أَسْتَغْفِرُ اللّٰهَ الْعَظِيْمَ الَّذِي لَٓا إِلَهَ إِلاَّ هُوَ الْحَيُّ الْقَيُّوْمُ وَ أَتُوْبُ إِلَيْهِ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
     {
       arabic:
         'اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ، كَمَا صَلَّيْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ، إِنَّكَ حَمِيدٌ مَجِيدٌ  اللَّهُمَّ بَارِكْ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ، كَمَا بَارَكْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ، إِنَّكَ حَمِيدٌ مَجِيدٌ',
       bosnian: '',
       counter: 0,
-      favorite: false
+      favorite: false,
     },
   ];
   dhikrsFromStorage = [];
@@ -132,15 +132,19 @@ export class TasbeehService {
       this.dhikrsFromStorage.filter((item) => {
         const currentDate = this.getCurrentDateAsString(new Date());
         if (item.date === currentDate) {
-          this.dhikrs = item.dhikrs.sort((a, b) => b.favorite - a.favorite);;
+          this.dhikrs = item.dhikrs.sort((a, b) => b.favorite - a.favorite);
         }
       });
     } else {
-      const obj = {
-        date: this.getCurrentDateAsString(new Date()),
-        dhikrs: this.dhikrs,
-      };
-      this.dhikrsFromStorage.push(obj);
+
+      for(let i = 1; i <= this.getDaysNumberOfCurrentMonth(); i++) {
+        const obj = {
+          date: this.getCurrentDateAsString(new Date(new Date().setDate(i))),
+          dhikrs: this.dhikrs,
+        };
+
+        this.dhikrsFromStorage.push(obj);
+      }
       localStorage.setItem('dhikrs', JSON.stringify(this.dhikrsFromStorage));
     }
   }
@@ -156,23 +160,15 @@ export class TasbeehService {
   }
 
   increaseCounter() {
-    this.selectedDhikr.counter++;
-    const obj = {
-      date: this.getCurrentDateAsString(new Date()),
-      dhikrs: this.dhikrs,
-    };
-    let added = false;
-    if (this.dhikrsFromStorage && !added) {
       this.dhikrsFromStorage.forEach((item) => {
         if (item.date === this.getCurrentDateAsString(new Date())) {
-          item.dhikrs = this.dhikrs;
-          added = true;
+          item.dhikrs.forEach(element => {
+            if(element.arabic === this.selectedDhikr.arabic){
+              element.counter ++;
+            }
+          });
         }
       });
-    }
-    if (!added) {
-      this.dhikrsFromStorage.push(obj);
-    }
     localStorage.setItem('dhikrs', JSON.stringify(this.dhikrsFromStorage));
     localStorage.setItem('selectedDhikr', JSON.stringify(this.selectedDhikr));
   }
@@ -189,9 +185,9 @@ export class TasbeehService {
   }
 
   toggleFavorite(dhikr, value: boolean) {
-    this.dhikrs.forEach(item => {
-      if(item.arabic === dhikr.arabic){
-        item.favorite = value? true: false;
+    this.dhikrs.forEach((item) => {
+      if (item.arabic === dhikr.arabic) {
+        item.favorite = value ? true : false;
       }
     });
     localStorage.setItem('dhikrs', JSON.stringify(this.dhikrsFromStorage));
@@ -199,29 +195,60 @@ export class TasbeehService {
   }
 
   getDhikrsByMonth(month: string) {
-    return this.dhikrsFromStorage.filter(item => this.getMonthFromDate(item.date) === month);
+    return this.dhikrsFromStorage.filter(
+      (item) => this.getMonthFromDate(item.date) === month
+    );
   }
 
-  getParticularDhikrByMonth(selectedDhikr: any, month: string){
-    const dhikrsByDate = this.dhikrsFromStorage.filter(item => this.getMonthFromDate(item.date) === month);
-    const dhikrs = []; const dates = [];
-    dhikrsByDate.forEach(item => {
+  getParticularDhikrByMonth(selectedDhikr: any, month: string) {
+    const dhikrsByDate = this.dhikrsFromStorage.filter(
+      (item) => this.getMonthFromDate(item.date) === month
+    );
+    const dhikrs = [];
+    const dates = [];
+    dhikrsByDate.forEach((item) => {
       dates.push(item.date);
-      item.dhikrs.forEach(dhikr => {
-        if(dhikr.arabic === selectedDhikr.arabic){
+      item.dhikrs.forEach((dhikr) => {
+        if (dhikr.arabic === selectedDhikr.arabic) {
           dhikrs.push(dhikr);
         }
       });
     });
     const obj = {
       labels: dates,
-      data: dhikrs.map(item => item.counter)
+      data: dhikrs.map((item) => item.counter),
     };
     return obj;
   }
 
-  getMonthFromDate(date: string){
+  getMonthFromDate(date: string) {
     const month = date.split('.')[1];
     return month;
+  }
+
+  getDaysNumberOfCurrentMonth() {
+    const currentMonth = new Date().getMonth() + 1;
+    let numberOfDaysInCurrentMonth;
+    if (
+      currentMonth === 1 ||
+      currentMonth === 3 ||
+      currentMonth === 5 ||
+      currentMonth === 7 ||
+      currentMonth === 8 ||
+      currentMonth === 10 ||
+      currentMonth === 12
+    ) {
+      numberOfDaysInCurrentMonth = 31;
+    } else if (
+      currentMonth === 4 ||
+      currentMonth === 6 ||
+      currentMonth === 9 ||
+      currentMonth === 11
+    ) {
+      numberOfDaysInCurrentMonth = 30;
+    } else if (currentMonth === 2) {
+      numberOfDaysInCurrentMonth = 28;
+    }
+    return numberOfDaysInCurrentMonth;
   }
 }
